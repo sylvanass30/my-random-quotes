95948645a8876a23384d04f627a9e2a13dbe2745
Nikita Kolyaskin <bigcleanerlol@gmail.com>
2025-01-28 23:34:43 +0700

    added more quotes made app in dark theme

--- script.js
+++ script.js
@@ -1,19 +1,61 @@
 const quotes = [
-    'The only way to do great work is to love what you do. - Steve Jobs',
-    'Innovation distinguishes between a leader and a follower. - Steve Jobs',
-    "Your time is limited, so don't waste it living someone else's life. - Steve Jobs",
-    'Im just a human, not a robot, i can make mistakes, so as you - Sasha Junin', 
-  ];
+  {
+    quote: 'The only way to do great work is to love what you do.',
+    author: 'Steve Jobs'
+  },
+  {
+    quote: 'Innovation distinguishes between a leader and a follower.',
+    author: 'Steve Jobs'
+  },
+  {
+    quote: 'Your time is limited, so don\'t waste it living someone else\'s life.',
+    author: 'Steve Jobs'
+  },
+  {
+    quote: 'I\'m just a human, not a robot, I can make mistakes, so as you.',
+    author: 'Sasha Junin'
+  },
+  {
+    quote: 'I am so clever that sometimes I don’t understand a single word of what I am saying.',
+    author: 'Oscar Wilde'
+  },
+  {
+    quote: 'I refuse to join any club that would have me as a member.',
+    author: 'Groucho Marx'
+  },
+  {
+    quote: 'I have not failed. I’ve just found 10,000 ways that won’t work.',
+    author: 'Thomas Edison'
+  },
+  {
+    quote: 'I’m sick of following my dreams. I’m just going to ask them where they’re going and hook up with them later.',
+    author: 'Mitch Hedberg'
+  },
+  {
+    quote: 'I always wanted to be somebody, but now I realize I should have been more specific.',
+    author: 'Lily Tomlin'
+  },
+  {
+    quote: 'I’m not great at the advice. Can I interest you in a sarcastic comment?',
+    author: 'Chandler Bing (Friends)'
+  }
+];
   
   const quoteElement = document.getElementById('quote');
+  const authorElement= document.getElementById('author')
   const generateBtn = document.getElementById('generate-btn');
-  
-  function generateRandomQuote() {
-    const randomIndex = Math.floor(Math.random() * quotes.length);
+  function generateRandomQuote(index) {
+    randomIndex = Math.floor(Math.random() * quotes.length);
+    // if (randomIndex===index) {
+    //   randomIndex+=1;
+    // }
     const randomQuote = quotes[randomIndex];
-    quoteElement.textContent = randomQuote;
+      quoteElement.textContent= `"${randomQuote.quote}"`;
+      authorElement.textContent=randomQuote.author;
+      authorElement.classList.remove('hidden');
+      authorElement.classList.add('visible');
+    
   }
 
   
   generateBtn.addEventListener('click', generateRandomQuote);
-  
